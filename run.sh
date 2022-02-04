fdb780aec6ae243f05f74d671c02aebc6e708df1
Mohammad <shafieirn@gmail.com>
2022-02-04 22:49:03 +0000

    install imageio config

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+pip3 install imageio
+python3 -c """import imageio
+imageio.plugins.freeimage.download()
+"""
 cd /mshvol/users/mohammad/optimization/DifferentiableSolver
 export PYTHONPATH=`pwd`:/mshvol/users/mohammad/cvgutils/
 python3 Flash_No_Flash/train.py
