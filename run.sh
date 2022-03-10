07dffc0ec495c29f15ee7564eb76cca19b9bdefb
Mohammad <moshafie@eng.ucsd.edu>
2022-03-10 08:52:55 -0800

    added lpips install

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cp /root/ssh_mount/id_rsa* /root/.ssh/
 chmod 400 ~/.ssh/id_rsa
 pip3 install imageio
 sudo apt-get -y install exiftool
-pip3 install PyExifTool piq
+pip3 install PyExifTool piq lpips
 python3 -c """import imageio
 imageio.plugins.freeimage.download()
 """
