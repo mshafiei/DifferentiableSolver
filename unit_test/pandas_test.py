6e381b4cd946b184705c5bb480f15e51fdb3065e
Mohammad <moshafie@eng.ucsd.edu>
2022-05-13 15:57:20 -0700

    updated pandas

--- unit_test/pandas_test.py
+++ unit_test/pandas_test.py
@@ -2,9 +2,7 @@ import pandas as pd
 import numpy as np
 from cvgutils.Utils import createTexTable, loadJson
 
-metrics = ['PSNR','SSIM']
-level_name = ['Level 4','Level 3','Level 2','Level 1']
-noise_levels = ['100x','50x','25x','12x']
+
 # row_title = ['U-Net',r'Ours w/o decomposition',' Ours with decomposition','deepFnF']
 #128
 # row_title = ['U-Net',
@@ -32,31 +30,55 @@ noise_levels = ['100x','50x','25x','12x']
 #fns.append('logger/fft_solver_largeds_relu/fft_helmholze-phi1-psi1e2/test/test_errors.txt')
 #fns.append('logger/fft_solver_largeds_relu/fft_helmholze-phi1e2-psi1/test/test_errors.txt')
 #fns.append('logger/fft_solver_largeds-relu/fft/test/test_errors.txt')
+def createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id):
+    ress = [loadJson(i) for i in method_fns]
+    data = np.zeros((len(method_name),len(noise_levels)*len(metrics)))
+    print(len(ress))
+    print(len(data))
+    for i, ress in enumerate(ress):
+        c = 0
+        for _, metric in enumerate(metrics_id):
+            for _, (name, level) in enumerate(zip(noise_levels_id,noise_levels)):
+                errval = ress[name][metric]
+                data[i,c] = float(errval)
+                c += 1
+    return data
 
-# ress = [loadJson(i) for i in fns]
-# data = np.zeros((len(row_title),len(noise_levels)*len(metrics)))
-# print(len(ress))
-# print(len(data))
-# for i, ress in enumerate(ress):
-#     c = 0
-#     for _, metric in enumerate(metrics):
-#         for _, (name, level) in enumerate(zip(level_name,noise_levels)):
-#             errval = ress[name][metric]
-#             data[i,c] = float(errval)
-#             c += 1
 
-row_title = ['U-Net',
+noise_levels = ['100x','50x','25x','12x']
+noise_levels_id = ['Level 4','Level 3','Level 2','Level 1']
+
+method_name = [
 r'Ours $\lambda_{\phi}=1,\lambda_{\psi}=1$ fixed',
-r'Ours $\lambda_{\phi}=1,\lambda_{\psi}=0.01$ learned',
-r'deepfnf']
+r'Ours $\lambda_{\phi}=0.01,\lambda_{\psi}=1$ learned',
+r'Ours $\lambda_{\phi}=1,\lambda_{\psi}=1$ learned',
+r'Ours $\lambda_{\phi}=1,\lambda_{\psi}=0.01$ learned',r'DeepFnF']
 
-data = np.zeros((len(row_title),len(noise_levels)*len(metrics)))
+method_fns = [
+'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1-fixed-128/test/test_errors.txt',
+'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1e2-psi1-128/test/test_errors.txt',
+'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1-128/test/test_errors.txt',
+'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1e2-128/test/test_errors.txt',
+'/mshvol2/users/mohammad/optimization/deepfnf_fork/logs/deepfnf/test']
 
-
-        
-data[0,:] = np.array([24.393, 26.134, 26.749, 27.233, 0.770, 0.811, 0.817, 0.842])
-data[1,:] = np.array([26.462, 28.650, 29.214, 30.514, 0.782, 0.823, 0.828, 0.854])
-data[2,:] = np.array([26.085, 28.579,29.252, 30.601, 0.773, 0.820,0.828,0.856])
-data[3,:] = np.array([28.018,29.404,29.642,30.818,0.807,0.830,0.831,0.854])
-tex = createTexTable(row_title,metrics,noise_levels,data)
+metrics = ['LPIPS-VGG','LPIPS-Alex']
+metrics_id = ['lpipsVGG','lpipsAlex']
+data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+tex = createTexTable(method_name,metrics,noise_levels,data)
+print('******* LPIPS-VGG, LPIPS-Alex *************')
 print(tex)
+
+metrics = ['PSNR','MSE']
+metrics_id = ['psnr','mse']
+data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+tex = createTexTable(method_name,metrics,noise_levels,data)
+print('******* PSNR, MSE *************')
+print(tex)
+
+metrics = ['MS-SSIM','SSIM']
+metrics_id = ['msssim','ssim']
+data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+tex = createTexTable(method_name,metrics,noise_levels,data)
+print('******* MS-SSIM, SSIM *************')
+print(tex)
+
