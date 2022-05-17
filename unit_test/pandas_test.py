6fda994b6d98631af49b44e2d2a7ffe95cd448d0
Mohammad <moshafie@eng.ucsd.edu>
2022-05-17 10:11:02 -0700

    fix

--- unit_test/pandas_test.py
+++ unit_test/pandas_test.py
@@ -52,20 +52,19 @@ method_name = [
 r'Ours $\lambda_{\phi}=1,\lambda_{\psi}=1$',
 r'Ours $\lambda^0_{\phi}=0.01,\lambda^0_{\psi}=1$',
 r'Ours $\lambda^0_{\phi}=1,\lambda^0_{\psi}=1$',
-r'Ours $\lambda^0_{\phi}=1,\lambda^0_{\psi}=0.01$',r'DeepFnF']
+r'Ours $\lambda^0_{\phi}=1,\lambda^0_{\psi}=0.01$']
 
 method_fns = [
 'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1-fixed-128/test/test_errors.txt',
 'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1e2-psi1-128/test/test_errors.txt',
 'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1-128/test/test_errors.txt',
-'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1e2-128/test/test_errors.txt',
-'/mshvol2/users/mohammad/optimization/deepfnf_fork/logs/deepfnf/test/test_errors.txt']
+'logger/fft_solver_largeds_relu-128/fft_helmholze-phi1-psi1e2-128/test/test_errors.txt']
 
 method_name += [
 r'Ours HD $\lambda^0_{\phi}=0.01,\lambda^0_{\psi}=0.01$',
 r'Ours HD $\lambda^0_{\phi}=0.01,\lambda^0_{\psi}=1$',
 r'Ours HD $\lambda^0_{\phi}=1,\lambda^0_{\psi}=0.01$',
-r'Ours HD $\lambda_{\phi}=1,\lambda_{\psi}=1$',r'DeepFnF']
+r'Ours HD $\lambda_{\phi}=1,\lambda_{\psi}=1$']
 
 method_fns += [
 'logger/fft-highdim-phi01-psi01-64/fft-highdim-phi01-psi01-64/test/test_errors.txt',
@@ -73,27 +72,29 @@ method_fns += [
 'logger/fft-highdim-phi1-psi01-64/fft-highdim-phi1-psi01-64/test/test_errors.txt',
 'logger/fft-highdim-phi1-psi1-fixed-64/fft-highdim-phi1-psi1-fixed-64/test/test_errors.txt']
 
+method_name += [r'DeepFnF']
+method_fns += ['/mshvol2/users/mohammad/optimization/deepfnf_fork/logs/deepfnf/test/test_errors.txt']
 
-metrics = ['LPIPS-VGG','LPIPS-Alex']
-metrics_id = ['lpipsVGG','lpipsAlex']
-data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
-tex = createTexTable(method_name,metrics,noise_levels,data)
-print('******* LPIPS-VGG, LPIPS-Alex *************')
-print(tex)
+# metrics = ['LPIPS-VGG','LPIPS-Alex']
+# metrics_id = ['lpipsVGG','lpipsAlex']
+# data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+# tex = createTexTable(method_name,metrics,noise_levels,data)
+# print('******* LPIPS-VGG, LPIPS-Alex *************')
+# print(tex)
 
-metrics = ['PSNR','MSE']
-metrics_id = ['psnr','mse']
-data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
-tex = createTexTable(method_name,metrics,noise_levels,data)
-print('******* PSNR, MSE *************')
-print(tex)
+# metrics = ['PSNR','MSE']
+# metrics_id = ['psnr','mse']
+# data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+# tex = createTexTable(method_name,metrics,noise_levels,data)
+# print('******* PSNR, MSE *************')
+# print(tex)
 
-metrics = ['MS-SSIM','SSIM']
-metrics_id = ['msssim','ssim']
-data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
-tex = createTexTable(method_name,metrics,noise_levels,data)
-print('******* MS-SSIM, SSIM *************')
-print(tex)
+# metrics = ['MS-SSIM','SSIM']
+# metrics_id = ['msssim','ssim']
+# data = createDataFile(method_fns,method_name,noise_levels,metrics,metrics_id,noise_levels_id)
+# tex = createTexTable(method_name,metrics,noise_levels,data)
+# print('******* MS-SSIM, SSIM *************')
+# print(tex)
 
 metrics = ['PSNR','SSIM']
 metrics_id = ['psnr','ssim']
