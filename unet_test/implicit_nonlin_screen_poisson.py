bedb1a36d1949c6d2f9dec8f62269f3994f26c6b
Mohammad <moshafie@eng.ucsd.edu>
2022-05-31 11:08:59 -0700

    fix

--- unet_test/implicit_nonlin_screen_poisson.py
+++ unet_test/implicit_nonlin_screen_poisson.py
@@ -224,7 +224,7 @@ def eval_visualize(params,batch,logger,mode,display,save_params,state,erreval=No
             imgs_sl['ambient'],labels_sl['ambient'] = imgs['ambient'],labels['ambient']
             imgs_sl['noisy'],labels_sl['noisy'] = imgs['noisy'],labels['noisy']
             imgs_sl['flash'],labels_sl['flash'] = imgs['flash'],labels['flash']
-            logger.addIndividualImages(imgs,labels,'image',dim_type='BHWC',mode=mode,text=r'$\lambda=%s, \delta$=%s'%(strlambda,strdelta),annotation=annotation,ltype='filesystem',ltype='html')
+            logger.addIndividualImages(imgs,labels,'image',dim_type='BHWC',mode=mode,text=r'$\lambda=%s, \delta$=%s'%(strlambda,strdelta),annotation=annotation,ltype='filesystem')
             # logger.addImage(imgs,labels,'image',dim_type='BHWC',mode=mode,text=r'$\lambda=%s, \delta$=%s'%(strlambda,strdelta),annotation=annotation)
             # logger.addImage(imgs_sl,labels_sl,'image',dim_type='BHWC',mode=mode,text=r'$\lambda=%s, \delta$=%s'%(strlambda,strdelta),annotation=annotation)
             # logger.addImage(imgs,labels,'image_inset',dim_type='BHWC',mode=mode,text=r'$\lambda=%s, \delta$=%s'%(strlambda,strdelta),addinset=True)
